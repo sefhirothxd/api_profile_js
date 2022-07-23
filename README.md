2e8a3c80f5b91004c684706bb293dfd4e754db67
bryan vera <bveracachay@gmail.com>
2022-07-23 15:55:57 -0500

    se agrega readme y se agrega la posibilidad de modificar username y hobbies

--- README.md
+++ README.md
@@ -28,3 +28,15 @@ descargar mediante zip desde github.
 | ![](https://raw.githubusercontent.com/alrra/browser-logos/main/src/chrome/chrome_48x48.png) | ![](https://raw.githubusercontent.com/alrra/browser-logos/main/src/firefox/firefox_48x48.png) | ![](https://raw.githubusercontent.com/alrra/browser-logos/main/src/safari/safari_48x48.png) | ![](https://raw.githubusercontent.com/alrra/browser-logos/main/src/opera/opera_48x48.png) | ![](https://raw.githubusercontent.com/alrra/browser-logos/main/src/edge/edge_48x48.png) | ![](https://raw.githubusercontent.com/alrra/browser-logos/master/src/archive/internet-explorer_9-11/internet-explorer_9-11_48x48.png) |
 | ------------------------------------------------------------------------------------------- | --------------------------------------------------------------------------------------------- | ------------------------------------------------------------------------------------------- | ----------------------------------------------------------------------------------------- | --------------------------------------------------------------------------------------- | ------------------------------------------------------------------------------------------------------------------------------------- |
 | Last ✔                                                                                      | Last ✔                                                                                        | Last ✔                                                                                      | Last ✔                                                                                    | Last ✔                                                                                  | Last ✔                                                                                                                                |
+
+## Contribución
+
+1. Crea un Fork del repositorio
+2. Clonar en tu maquina mediante git clone https://github.com/sefhirothxd/api_profile_js.git
+3. Crear una nueva rama
+4. Realiza tus cambios
+5. Manda tu pull request
+
+## License
+
+MIT
--- js/main.js
+++ js/main.js
@@ -1,15 +1,35 @@
 const form = document.querySelector('.form');
+const formModal = document.querySelector('.modal-body');
 const containerMain = document.querySelector('.container');
+const containerModal = document.querySelector('.modal-container');
+const btnClose = document.querySelector('.btn-close-modal');
+let emailInput = '';
+let discordInput = '';
+let userNameGet = '';
+let hobbiesGet = '';
 
 form.addEventListener('submit', (e) => {
+  console.log('me ejecuto');
   e.preventDefault();
   console.log('email: ', e.target[0].value);
-  const email = e.target[0].value;
+  emailInput = e.target[0].value;
   console.log('discord:', e.target[1].value);
-  const discord = e.target[1].value;
-  getInfoLarnu(email, discord);
+  discordInput = e.target[1].value;
+  getInfoLarnu(emailInput, discordInput);
 });
-
+formModal.addEventListener('submit', (e) => {
+  e.preventDefault();
+  console.log(e.target[0]);
+  const inputHobbies = e.target[1].value;
+  const inputDiscordUser = e.target[2].value;
+  console.log(inputHobbies, 'hobbies');
+  console.log(inputHobbies, 'discord user');
+  patchInfoLarnu(inputHobbies, inputDiscordUser);
+});
+btnClose.addEventListener('click', (e) => {
+  e.preventDefault();
+  containerModal.style.display = 'none';
+});
 const getInfoLarnu = async (email, discord) => {
   const res = await axios
     .get('https://larnu-api-upy5mhs63a-rj.a.run.app/api/v1/bootcamp/profile', {
@@ -23,27 +43,56 @@ const getInfoLarnu = async (email, discord) => {
       console.log(err);
       alert('El correo o el discord no existe');
     });
-  console.log(res?.data);
   if (res?.data !== undefined) {
+    userNameGet = res.data.discordUsername;
+    hobbiesGet = res.data.hobbies;
     getCard(res.data);
   } else {
     console.log('El correo o el discord no existe');
   }
 };
 
+const patchInfoLarnu = async (hobbies, userName) => {
+  const res = await axios
+    .patch(
+      'https://larnu-api-upy5mhs63a-rj.a.run.app/api/v1/bootcamp/profile',
+      {
+        discordUsername: userName || userNameGet,
+        hobbies: hobbies || hobbiesGet,
+      },
+      {
+        headers: {
+          Email: emailInput,
+          'Discord-id': discordInput,
+        },
+      }
+    )
+    .then((res) => res)
+    .catch((err) => {
+      console.log(err);
+      alert('error del patch');
+    });
+  if (res?.data !== undefined) {
+    getCard(res.data);
+  } else {
+    console.log('El correo o el discord no existe');
+  }
+  containerModal.style.display = 'none';
+};
+
 const getCard = async (data) => {
   console.log(data);
-
   const createdAt = new Date(data.user.createdAt).toLocaleString();
   const LastLogin = new Date(data.user.lastLogin).toLocaleString();
 
   containerMain.innerHTML = `
   <div class="container-card">
   <div class="container-arrow">
-    <img src="./img/arrow-white.svg" alt="arrow left" />
+    <img class="icon-arrow" src="./img/arrow-white.svg" alt="arrow left" />
+    <button class="btn-edit">Editar</button>
   </div>
   <div class="container-card__info">
-    <img src="https://i.pravatar.cc/300" alt="avatar" />
+    <img src="${data.user.avatar}" alt="avatar" />
     <div class="info-content">
       <div class="info-description">
         <p>Full Name</p>
@@ -69,14 +118,23 @@ const getCard = async (data) => {
         <p>Discord Nick</p>
         <p>${data.discordUsername}</p>
       </div>
+      <div class="info-description">
+        <p>Hobbies</p>
+        <p>${data.hobbies}</p>
+      </div>
     </div>
   </div>
 </div>
   `;
 
-  const containerArrow = document.querySelector('.container-arrow');
+  const containerArrow = document.querySelector('.icon-arrow');
 
   containerArrow.addEventListener('click', () => {
     window.location.reload();
+  });
+  const btnEdit = document.querySelector('.btn-edit');
+
+  btnEdit.addEventListener('click', () => {
+    containerModal.style.display = 'flex';
   });
 };
